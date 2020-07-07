a0b13c940089c2df3bbaffc048240a424c7badac
kromke <kromkemail@yandex.ru>
2020-07-07 22:41:51 +0300

    first commit

---
+++ src/test/java/org/test/listofvacation/ListOfVacationApplicationTests.java
@@ -0,0 +1,13 @@
+package org.test.listofvacation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ListOfVacationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
