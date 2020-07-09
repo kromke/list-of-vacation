466c7bbf1989dc12eb161131c75c35dc277bb6b5
kromke <kromkemail@yandex.ru>
2020-07-09 17:04:47 +0300

    many changes

--- src/main/java/org/test/listofvacation/controller/AppController.java
+++ src/main/java/org/test/listofvacation/controller/AppController.java
@@ -1,9 +1,16 @@
 package org.test.listofvacation.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.test.listofvacation.dto.EmployeeDataProvider;
+
+import java.util.List;
 
 @RestController
 public class AppController {
 
-
+    @GetMapping("/employees")
+    public EmployeeDataProvider getEmployeesList() {
+        return null;
+    }
 }
---
+++ src/main/java/org/test/listofvacation/dto/EmployeeDataProvider.java
@@ -0,0 +1,12 @@
+package org.test.listofvacation.dto;
+
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@JsonPropertyOrder({
+        "employees"
+})
+public class EmployeeDataProvider {
+}
